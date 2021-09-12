178d964b98cfcf2c4f4e25a53df9d678b5af02c2
Belley <Belleyy@users.noreply.github.com>
2021-09-12 17:46:09 +0200

    Using xpath instead of graphql
    Probably more safe

--- VLC Userscript.js
+++ VLC Userscript.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Stash addons
+// @name        Stash Userscript (VLC)
 // @description Userscript for Stash
 // @match       *localhost:9999/*
 // @require  	https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
@@ -47,11 +47,14 @@
                     vlc_div.innerHTML = '<a class="minimal settings-button btn btn-primary" id="userscript_vlc" style="z-index: 1;position: relative;float: left;border-radius: 50px;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-btn" viewBox="0 0 16 16"><path d="M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"></path><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"></path></svg></a>';
                     jwplayer_div.insertBefore(vlc_div, document.getElementsByClassName('jw-aspect jw-reset')[0]);
                     document.getElementById("userscript_vlc").addEventListener("click", function () {
-                        RequestData().then(response_json => {
-                            var findScene = response_json.data.findScene;
-                            document.location.href = 'vlcs' + findScene.path;
-                        });
+                        var path_text = getElementByXpath('//a[contains(@href,"file://")]/@href')
+                        if (path_text){
+                            document.location.href = 'vlcs:' + path_text.textContent.replace("file://","");
+                        }else{
+                            console.error("Error getting the path by using xPath")
+                        }
                     }, false);
+
                 }
             });
         } else {
@@ -158,24 +161,7 @@
             }
         }, time);
     }
-    function RequestData(id) {
-        id = (typeof id !== 'undefined') ? id : window.location.pathname.split("/").pop();
-        // remove possible parameter
-        id = id.replace(/\?.+/gm, "")
-        return fetch('/graphql', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({
-                operationName: 'FindScene',
-                variables: {
-                    'id': id
-                },
-                query: "query FindScene($id: ID!, $checksum: String) {  findScene(id: $id, checksum: $checksum) {    ...SceneData    __typename  }  sceneMarkerTags(scene_id: $id) {    tag {      id      name      __typename    }    scene_markers {      ...SceneMarkerData      __typename    }    __typename  }}fragment SceneData on Scene {  id  checksum  oshash  title  details  url  date  rating  o_counter  organized  path  phash  interactive  file {    size    duration    video_codec    audio_codec    width    height    framerate    bitrate    __typename  }  paths {    screenshot    preview    stream    webp    vtt    chapters_vtt    sprite    funscript    __typename  }  scene_markers {    ...SceneMarkerData    __typename  }  galleries {    ...SlimGalleryData    __typename  }  studio {    ...SlimStudioData    __typename  }  movies {    movie {      ...MovieData      __typename    }    scene_index    __typename  }  tags {    ...SlimTagData    __typename  }  performers {    ...PerformerData    __typename  }  stash_ids {    endpoint    stash_id    __typename  }  __typename}fragment SceneMarkerData on SceneMarker {  id  title  seconds  stream  preview  scene {    id    __typename  }  primary_tag {    id    name    aliases    __typename  }  tags {    id    name    aliases    __typename  }  __typename}fragment SlimGalleryData on Gallery {  id  checksum  path  title  date  url  details  rating  organized  image_count  cover {    file {      size      width      height      __typename    }    paths {      thumbnail      __typename    }    __typename  }  studio {    id    name    image_path    __typename  }  tags {    id    name    __typename  }  performers {    id    name    gender    favorite    image_path    __typename  }  scenes {    id    title    path    __typename  }  __typename}fragment SlimStudioData on Studio {  id  name  image_path  stash_ids {    endpoint    stash_id    __typename  }  parent_studio {    id    __typename  }  details  rating  aliases  __typename}fragment MovieData on Movie {  id  checksum  name  aliases  duration  date  rating  director  studio {    ...SlimStudioData    __typename  }  synopsis  url  front_image_path  back_image_path  scene_count  __typename}fragment SlimTagData on Tag {  id  name  aliases  image_path  __typename}fragment PerformerData on Performer {  id  checksum  name  url  gender  twitter  instagram  birthdate  ethnicity  country  eye_color  height  measurements  fake_tits  career_length  tattoos  piercings  aliases  favorite  image_path  scene_count  image_count  gallery_count  tags {    ...SlimTagData    __typename  }  stash_ids {    stash_id    endpoint    __typename  }  rating  details  death_date  hair_color  weight  __typename}"
-            })
-        })
-            .then(r => r.json());
+    function getElementByXpath(xpath) {
+        return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     }
 })();
